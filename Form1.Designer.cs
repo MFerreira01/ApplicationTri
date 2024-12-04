2dc2e07bfaf4f8f39236098a82030fd45ffbfcf5
Mariana FERREIRA <marianaf0108@gmail.com>
2024-12-04 22:00:19 +0100

    Premier commit
    On a l'acquisition de l'image.
    J'ai fais une fonction captureImage pour prendre une image pour faire le traitement -> à tester.
    Je commence a inclure la DLL image.
    
    Commit en cas d'échec de l'intégration de la DLL.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ApplicationTri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbImageCam = new System.Windows.Forms.PictureBox();
+            this.BoutonACQ = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonInit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImageCam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImageCam
+            // 
+            this.pbImageCam.Location = new System.Drawing.Point(30, 12);
+            this.pbImageCam.Name = "pbImageCam";
+            this.pbImageCam.Size = new System.Drawing.Size(687, 581);
+            this.pbImageCam.TabIndex = 0;
+            this.pbImageCam.TabStop = false;
+            // 
+            // BoutonACQ
+            // 
+            this.BoutonACQ.Location = new System.Drawing.Point(875, 194);
+            this.BoutonACQ.Name = "BoutonACQ";
+            this.BoutonACQ.Size = new System.Drawing.Size(133, 66);
+            this.BoutonACQ.TabIndex = 1;
+            this.BoutonACQ.Text = "ACQ";
+            this.BoutonACQ.UseVisualStyleBackColor = true;
+            this.BoutonACQ.Click += new System.EventHandler(this.BoutonACQ_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(848, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.OnTimer);
+            // 
+            // buttonInit
+            // 
+            this.buttonInit.Location = new System.Drawing.Point(875, 103);
+            this.buttonInit.Name = "buttonInit";
+            this.buttonInit.Size = new System.Drawing.Size(136, 66);
+            this.buttonInit.TabIndex = 3;
+            this.buttonInit.Text = "init";
+            this.buttonInit.UseVisualStyleBackColor = true;
+            this.buttonInit.Click += new System.EventHandler(this.buttonInit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2054, 834);
+            this.Controls.Add(this.buttonInit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BoutonACQ);
+            this.Controls.Add(this.pbImageCam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.OnLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImageCam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImageCam;
+        private System.Windows.Forms.Button BoutonACQ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonInit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace ApplicationTri
+{
+    public partial class Form1 : Form
+    {
+        smcs.IDevice m_device;
+        Rectangle m_rect;
+        PixelFormat m_pixelFormat;
+        UInt32 m_pixelType;
+
+        smcs.IImageProcAPI m_imageProcApi;
+        smcs.IAlgorithm m_changeBitDepthAlg;
+        smcs.IParams m_changeBitDepthParams;
+        smcs.IImageBitmap m_changeBitDepthBitmap;
+
+        private Bitmap capturedImage; // Variable pour stocker l'image capturée
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            // initialize GigEVision API
+            smcs.CameraSuite.InitCameraAPI();
+            smcs.ICameraAPI smcsVisionApi = smcs.CameraSuite.GetCameraAPI();
+
+            smcs.CameraSuite.InitImageProcAPI();
+            m_imageProcApi = smcs.CameraSuite.GetImageProcAPI();
+
+            if (!smcsVisionApi.IsUsingKernelDriver())
+            {
+                Text = Text + " (Warning: Smartek Filter Driver not loaded.)";
+            }
+
+            m_changeBitDepthAlg = m_imageProcApi.GetAlgorithmByName("ChangeBitDepth");
+            m_changeBitDepthAlg.CreateParams(ref m_changeBitDepthParams);
+            m_imageProcApi.CreateBitmap(ref m_changeBitDepthBitmap);
+
+
+            label1.Text = "No camera connected";
+
+            // discover all devices on network
+            smcsVisionApi.FindAllDevices(3.0);
+            smcs.IDevice[] devices = smcsVisionApi.GetAllDevices();
+            if (devices.Length <= 0) return;
+            m_device = devices[0];
+
+            if (m_device == null || !m_device.Connect()) return;
+
+            label1.Text = "Camera address:" + Common.IpAddrToString(m_device.GetIpAddress());
+
+            // disable trigger mode
+            bool status = m_device.SetStringNodeValue("TriggerMode", "Off");
+            // set continuous acquisition mode
+            status = m_device.SetStringNodeValue("AcquisitionMode", "Continuous");
+            // start acquisition
+            status = m_device.SetIntegerNodeValue("TLParamsLocked", 1);
+            status = m_device.CommandNodeExecute("AcquisitionStart");
+            timer1.Enabled = true;
+        }
+
+        private void OnTimer(object sender, EventArgs e)
+        {
+            smcs.IImageInfo imageInfo = null;
+            if (!m_device.GetImageInfo(ref imageInfo)) return;
+
+
+            UInt32 pixelType;
+            imageInfo.GetPixelType(out pixelType);
+            var depth = smcs.CameraSuite.GvspGetBitsDepth((smcs.GVSP_PIXEL_TYPES)pixelType);
+            var image = (smcs.IImageBitmap)imageInfo;
+
+            if (depth > 8)
+            {
+                // to show image we change bit depth to 8 bit
+                m_changeBitDepthParams.SetIntegerNodeValue("BitDepth", 8);
+                m_imageProcApi.ExecuteAlgorithm(m_changeBitDepthAlg, image, m_changeBitDepthBitmap, m_changeBitDepthParams, null);
+                image = m_changeBitDepthBitmap;
+            }
+
+
+
+            Bitmap bitmap = (Bitmap)pbImageCam.Image;
+            BitmapData bd = null;
+
+            ImageUtils.CopyToBitmap(image, ref bitmap, ref bd, ref m_pixelFormat, ref m_rect, ref m_pixelType);
+
+            if (bitmap != null)
+            {
+                pbImageCam.Height = bitmap.Height;
+                pbImageCam.Width = bitmap.Width;
+                pbImageCam.Image = bitmap;
+            }
+
+            // display image
+            if (bd != null)
+                bitmap.UnlockBits(bd);
+
+            pbImageCam.Invalidate();
+            // remove (pop) image from image buffer
+            m_device.PopImage(imageInfo);
+            // empty buffer
+            m_device.ClearImageBuffer();
+        }
+
+        private void OnClosed(object sender, FormClosedEventArgs e)
+        {
+            timer1.Enabled = false;
+            if (m_device != null && m_device.IsConnected())
+            {
+                m_device.CommandNodeExecute("AcquisitionStop");
+                m_device.SetIntegerNodeValue("TLParamsLocked", 0);
+                m_device.Disconnect();
+            }
+
+            m_changeBitDepthAlg.DestroyParams(m_changeBitDepthParams);
+            m_imageProcApi.DestroyBitmap(m_changeBitDepthBitmap);
+
+            smcs.CameraSuite.ExitImageProcAPI();
+            smcs.CameraSuite.ExitCameraAPI();
+        }
+        private Bitmap CaptureImage()
+        {
+            smcs.IImageInfo imageInfo = null;
+            if (!m_device.GetImageInfo(ref imageInfo)) return null;
+
+            UInt32 pixelType;
+            imageInfo.GetPixelType(out pixelType);
+            var depth = smcs.CameraSuite.GvspGetBitsDepth((smcs.GVSP_PIXEL_TYPES)pixelType);
+            var image = (smcs.IImageBitmap)imageInfo;
+
+            if (depth > 8)
+            {
+                // Convertit en 8 bits pour l'affichage
+                m_changeBitDepthParams.SetIntegerNodeValue("BitDepth", 8);
+                m_imageProcApi.ExecuteAlgorithm(m_changeBitDepthAlg, image, m_changeBitDepthBitmap, m_changeBitDepthParams, null);
+                image = m_changeBitDepthBitmap;
+            }
+
+            Bitmap bitmap = null;
+            BitmapData bd = null;
+
+            ImageUtils.CopyToBitmap(image, ref bitmap, ref bd, ref m_pixelFormat, ref m_rect, ref m_pixelType);
+
+            if (bd != null)
+                bitmap.UnlockBits(bd);
+
+            // Libère l'image de la mémoire tampon de la caméra
+            m_device.PopImage(imageInfo);
+            m_device.ClearImageBuffer();
+
+            return bitmap;
+        }
+
+        private void BoutonACQ_Click(object sender, EventArgs e)
+        {
+            // Capture une image
+            capturedImage = CaptureImage();
+
+            // Affiche l'image capturée dans le PictureBox
+            if (capturedImage != null)
+            {
+                pbImageCam.Image = capturedImage;
+                MessageBox.Show("Image capturée et stockée en mémoire !");
+
+
+               /* // Calcul de l'histogramme
+                var histogramData = Histogramme(capturedImage);
+
+                // Affichage ou utilisation de l'histogramme
+                AfficherHistogramme(histogramData);*/
+            }
+            else
+            {
+                MessageBox.Show("Échec de la capture de l'image.");
+            }
+        }
+
+/*
+        private void BoutonACQ_Click(object sender, EventArgs e)
+        {
+            *//*Bitmap capturedImage = CaptureImage();
+            pbImageCam.Image = capturedImage;*//*
+
+        }*/
+
+        private void buttonInit_Click(object sender, EventArgs e)
+        {
+            if (m_device !=null)
+            {
+                label1.Text = "Adresse IP:" + Common.IpAddrToString(m_device.GetIpAddress());
+            }
+            else
+            {
+                label1.Text = "pas connecté";
+            }
+
+        }
+
+    }
+}
