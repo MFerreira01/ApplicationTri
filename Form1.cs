7fc581b9ec2130bcb499db7f08b486acfd875f0c
Mariana FERREIRA <marianaf0108@gmail.com>
2024-12-05 17:18:31 +0100

    Commit réparation acquisition
    Avant ce commit, l'acquisition fonctionne mal. Parfois elle fonctionne, parfois non. On ajoute une fonction delegate pour reccupere l'image lorsqu'elle est disponible.

--- Form1.cs
+++ Form1.cs
@@ -137,6 +137,18 @@ namespace ApplicationTri
             smcs.CameraSuite.ExitImageProcAPI();
             smcs.CameraSuite.ExitCameraAPI();
         }
+
+        private delegate void affImage(PictureBox Pbx, Bitmap bmp);
+        private void GetImg(PictureBox Pb, Bitmap btmp)
+        {
+	        if (Pb.InvokeRequired)
+	        {
+		        affImage d;
+		        d = new affImage(GetImg);
+		        this.Invoke(d, new object[] { Pb, btmp });
+	        }
+	        else btmp = new Bitmap(Pb.Image);
+        }
         private Bitmap CaptureImage()
         {
             smcs.IImageInfo imageInfo = null;
@@ -220,7 +232,7 @@ namespace ApplicationTri
             // Affiche l'image capturée dans le PictureBox
             if (capturedImage != null)
             {
-                pbImageCam.Image = capturedImage;
+                GetImg(pbImageCam, capturedImage);
                 pbImageCapture.Image= capturedImage;
                 MessageBox.Show("Image capturée et stockée en mémoire !");
 
@@ -228,7 +240,7 @@ namespace ApplicationTri
 
                 if(moyenneNDG>128)
                 {
-                    labelDécision.Text = "Décision : Objet blANc";
+                    labelDécision.Text = "Décision : Objet blanc";
                 }
                 else
                 {
